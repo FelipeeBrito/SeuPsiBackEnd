45201b9bce85a9c08c9a0f9ec5eb1fa80e07b3d9
FelipeeBrito <felipe_maycon@hotmail.com>
2023-07-17 13:41:05 -0300

    feat(Table): Created costumers and schedule table

---
+++ prisma/migrations/20230717160131_schedules_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "schedules" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" TEXT NOT NULL,
+    "professional" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "situation" TEXT NOT NULL
+);
